a962f2fa9eb224e416bb36a1693a0dfee549dc0d
Ардак <ardakallplay@gmail.com>
2024-11-17 21:28:13 +0500

    init

---
+++ src/main/java/com/example/nazvaniye3/Nazvaniye3Application.java
@@ -0,0 +1,13 @@
+package com.example.nazvaniye3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nazvaniye3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nazvaniye3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nazvaniye3/Nazvaniye3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nazvaniye3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nazvaniye3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
